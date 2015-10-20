c295d2ec513b286a333b20f1e515527d0ec6dae4
jacek <jacek.malolepszy@itcrowd.pl>
2015-10-20 23:28:28 +0200

    Get email addresses from main page

--- src/main/java/spiders/Pkt2015.java
+++ src/main/java/spiders/Pkt2015.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 /**
  * @author jacek.malolepszy@hp.com
  * @since 2015-10-20
+ * 1h
  */
 public class Pkt2015 {
 
@@ -20,19 +21,26 @@ public class Pkt2015 {
 
         String initialUrl = "http://www.pkt.pl/artykuly-papiernicze-1/20-1/";
 
-        Document doc = Jsoup.connect(initialUrl).get();
-        Elements categoryLinkList = doc.select(".box-category_items a");
+        Document mainPage = Jsoup.connect(initialUrl).get();
+        Elements categoryLinkList = mainPage.select(".box-category_items a");
 
         for (Element item : categoryLinkList) {
-            String subCategoryUrl = baseUrl + "/" + item.attr("href");
+            String categoryUrl = baseUrl + "/" + item.attr("href");
 
-            Document subDoc = Jsoup.connect(subCategoryUrl).get();
+            Document categoryPage = Jsoup.connect(categoryUrl).get();
 
-            Elements subCategoryLinkList = subDoc.select(".box-categories li a");
+            Elements subCategoryLinkList = categoryPage.select(".box-categories li a");
 
             System.out.println(item.attr("href"));
             for (Element element : subCategoryLinkList) {
-                System.out.println("\t" + element.attr("href"));
+                String subCategoryUrl = baseUrl + element.attr("href");
+
+                Document concretePage = Jsoup.connect(subCategoryUrl).get();
+                Elements emailElements = concretePage.select(".box-content .call--email span");
+
+                for (Element emailElement : emailElements) {
+                    System.out.println(emailElement.attr("title"));
+                }
             }
         }
 
