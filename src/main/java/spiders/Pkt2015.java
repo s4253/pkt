5630ce7825e2e78bb7a9ba0ba43ecfdc288cd6fe
jacek <jacek.malolepszy@itcrowd.pl>
2015-10-20 23:02:41 +0200

    Get links at first page

--- src/main/java/spiders/Pkt2015.java
+++ src/main/java/spiders/Pkt2015.java
@@ -1,8 +1,28 @@
 package spiders;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 /**
  * @author jacek.malolepszy@hp.com
  * @since 2015-10-20
  */
 public class Pkt2015 {
+
+    public static void main(String[] args) throws IOException {
+
+        String baseUrl = "http://www.pkt.pl/artykuly-papiernicze-1/20-1/";
+
+        Document doc = Jsoup.connect(baseUrl).get();
+        Elements linkList = doc.select(".box-category_items a");
+
+        for (Element item : linkList)
+            System.out.println(item.attr("href"));
+
+    }
 }
